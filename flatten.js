352ac6e82aca48445f4a6e66df757a93cf93facd
yangfuzhang <yangfuzhang0720@126.com>
2020-12-29 06:53:50 +0800

    add：new file

---
+++ flatten.js
@@ -0,0 +1,43 @@
+// 方法一：循环递归
+function flatten(arr) {
+	let result = []
+
+	for(let i = 0; i < arr.length; i++) {
+		if(Array.isArray(arr[i])) {
+			result = result.concat(flatten(arr[i]))
+		} else {
+			result.push(arr[i])
+		}
+	}
+
+	return result
+}
+
+let arr = [1, [2, [3, 5]], 6, [9, 7]]
+
+flatten(arr)
+
+// 方法二：reduce
+function flatten(arr) {
+	return arr.reduce(function(prev, next) {
+		return prev.concat(Array.isArray(next) ? flatten(next) : next)
+	}, [])
+}
+
+let arr = [1, [2, [3, 5]], 6, [9, 7]]
+
+flatten(arr)
+
+
+//ES6 API
+function flatten(arr) {
+	while (arr.some(item => Array.isArray(item))) {
+        arr = [].concat(...arr)
+    }
+
+    return arr
+}
+
+let arr = [1, [2, [3, 5]], 6, [9, 7]]
+
+flatten(arr)
