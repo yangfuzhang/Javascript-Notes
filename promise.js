9cccfb478e220009de65b4a5c9bf64d7174ae94b
yangfuzhang <yangfuzhang0720@126.com>
2023-08-10 20:14:32 +0800

    amend promise.js

--- promise.js
+++ promise.js
@@ -15,30 +15,30 @@ function Mypromise(callback) {
 	that.onRejectedCallbacks = []
 
 	that.resolve = function (value) {
-        setTimeout(() => {
-        	if(that.state === PENDING) {
-        		that.state = FULFILLED
-        	    that.value = value
-        	    that.onResolvedCallbacks.forEach(cb => cb())
-        	}
-        }, 0)
+		setTimeout(() => {
+			if(that.state === PENDING) {
+				that.state = FULFILLED
+				that.value = value
+				that.onResolvedCallbacks.forEach(cb => cb())
+			}
+		}, 0)
 	}
 
 	that.reject = function (value) {
 		setTimeout(() => {
 			if(that.state === PENDING) {
-        		that.state = REJECTED
-        	    that.value = value
-        	    that.onRejectedCallbacks.forEach(cb => cb())
-        	}
+        that.state = REJECTED
+        that.value = value
+        that.onRejectedCallbacks.forEach(cb => cb())
+      }
 		},0)
 	}
     
-    try {
-        callback(that.resolve, that.reject)
-    } catch (err) {
-        that.reject(err)
-    }
+	try {
+		callback(that.resolve, that.reject)
+	} catch (err) {
+		that.reject(err)
+	}
 }
 
 Mypromise.prototype.then = function(onFullfilled, onRejected) {
@@ -52,9 +52,9 @@ Mypromise.prototype.then = function(onFullfilled, onRejected) {
         promise2 = new Mypromise(function(resolve, reject) {
             that.onResolvedCallbacks.push(function() {
             	try {
-                    var x = onFullfilled(that.value)
+                var x = onFullfilled(that.value)
 
-            	    resolveProcedure(promise2, x, resolve, reject)
+            	  resolveProcedure(promise2, x, resolve, reject)
             	} catch (err) {
             		reject(err)
             	}
@@ -74,13 +74,13 @@ Mypromise.prototype.then = function(onFullfilled, onRejected) {
 
     if(that.state === FULFILLED) {
     	promise2 = new Mypromise(function(resolve, reject) {
-            try {
-            	var x = onFulfilled(that.value)
+				try {
+					var x = onFulfilled(that.value)
 
-                resolveProcedure(promise2, x, resolve, reject)
-            } catch (err) {
-                reject(err)
-            }
+						resolveProcedure(promise2, x, resolve, reject)
+				} catch (err) {
+						reject(err)
+				}
     	})
 
     	return promise2
@@ -88,13 +88,13 @@ Mypromise.prototype.then = function(onFullfilled, onRejected) {
 
     if(that.state === REJECTED) {
     	promise2 = new Mypromise(function(resolve, reject) {
-            try {
-            	var x = onRejected(that.value)
+				try {
+					var x = onRejected(that.value)
 
-                resolveProcedure(promise2, x, resolve, reject)
-            } catch (err) {
-                reject(err)
-            }
+						resolveProcedure(promise2, x, resolve, reject)
+				} catch (err) {
+						reject(err)
+				}
     	})
 
     	return promise2
@@ -113,7 +113,7 @@ function resolveProcedure(promise2, x, resolve, reject) {
 			}, reject)
 		} else {
 			// 此时resolve参数即为then方法的onFullfilled
-            x.then(resolve, reject)
+      x.then(resolve, reject)
 		}
 
 		return
@@ -125,19 +125,16 @@ function resolveProcedure(promise2, x, resolve, reject) {
 var mypromise = new Mypromise(function(resolve, reject) {
 	setTimeout(function() {
 		// 第一次调用resolve，最外层promise状态变成fullfilled，执行then中的回调函数
-        resolve('在第一个then中打印')
+    resolve('在第一个then中打印')
 	}, 1000)
 })
 
 var mypromise1 = mypromise.then(function(value) {
-        console.log(value)
-
-        return new Mypromise(function(resolve, reject) {
-        	setTimeout(function() {
-        		resolve('在第二个then中打印')
-        	}, 1000)
-        })
-	})
-    .then(function(value) {
+		return new Mypromise(function(resolve, reject) {
+			setTimeout(function() {
+				resolve('在第二个then中打印')
+			}, 1000)
+		})
+	}).then(function(value) {
 		console.log(value)
 	})
